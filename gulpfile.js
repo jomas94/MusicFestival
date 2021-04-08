d05fa5e3964235daafd5269977aad9cdd7132148
jomas94 <jomariojsa@gmail.com>
2021-04-08 17:28:10 +0200

    gulp-imagemin,layouts

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 
 const { src, dest, series, parallel, watch } = require('gulp');
 const sass = require('gulp-sass');
-
+const imagemin = require('gulp-imagemin');
 sass.compiler = require('dart-sass'); 
 
 function compilarSASS() {
@@ -12,7 +12,7 @@ function compilarSASS() {
     
 }
 
-function minificar() {
+function minificarcss() {
     return src("./src/scss/app.scss")
     .pipe(sass({
         outputStyle:'compressed'
@@ -21,14 +21,22 @@ function minificar() {
     
 }
 
+function imagenes() {
+    return src('src/img/**/*')
+    .pipe(imagemin())
+    .pipe(dest('./build/img'))
+    
+}
+
 function watchingFiles() {
-    watch("./src/scss/**/*.scss",compilarSASS)
+    watch("./src/scss/**/*.scss",compilarSASS);
     
 }
 
 
 exports.compilarSASS = compilarSASS;
-exports.minificar = minificar;
+exports.minificar = minificarcss;
+exports.imagenes = imagenes;
 exports.watchingFiles = watchingFiles;
 //exports.default = series(compilarSASS); con default ejecutará con tan solo GULP en el cmd
 //exports.default = parallel(compilarSASS); todas las funciones empezarán a la vez
