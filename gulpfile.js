554357a1dba4a59744c642d33bec57f31a3d32cb
jomas94 <jomariojsa@gmail.com>
2021-04-08 12:10:45 +0200

    index,globales,variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,34 @@
 
-const { src, dest, series, parallel } = require('gulp');
+const { src, dest, series, parallel, watch } = require('gulp');
 const sass = require('gulp-sass');
 
 sass.compiler = require('dart-sass'); 
 
 function compilarSASS() {
+    
     return src("./src/scss/app.scss")
     .pipe(sass())
     .pipe( dest("./build/css") );
+    
 }
 
+function minificar() {
+    return src("./src/scss/app.scss")
+    .pipe(sass({
+        outputStyle:'compressed'
+    }))
+    .pipe( dest("./build/css") );
+    
+}
+
+function watchingFiles() {
+    watch("./src/scss/**/*.scss",compilarSASS)
+    
+}
+
+
 exports.compilarSASS = compilarSASS;
-//exports.default = series(compilarSASS);
+exports.minificar = minificar;
+exports.watchingFiles = watchingFiles;
+//exports.default = series(compilarSASS); con default ejecutará con tan solo GULP en el cmd
+//exports.default = parallel(compilarSASS); todas las funciones empezarán a la vez
